61a75c07975a94c4a9a23e241b6734794ec8c3b8
enricbertrandiaz <enricbertrandiaz@gmail.com>
2021-02-21 18:52:18 +0100

    First Springboot commit

---
+++ src/main/java/com/bertran/pruebatrabajo/PruebaTrabajoApplication.java
@@ -0,0 +1,13 @@
+package com.bertran.pruebatrabajo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTrabajoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTrabajoApplication.class, args);
+	}
+
+}
